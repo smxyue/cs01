8ecc50ab61b1a7c639efda480a325afe99a7689f
过客 <smxyue@foxmail.com>
2022-09-26 16:52:48 +0800

    添加项目文件。

---
+++ cs01/cs01.cpp
@@ -0,0 +1,63 @@
+﻿// cs01.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+char matrix[10][10];
+void initMatrix()
+{
+    srand(int(time(0)));
+    for (int i = 0;i < 10;i++)
+    {
+        for (int j = 0;j < 10;j++)
+        {
+            int val = rand();
+            if (val % 2 == 0)
+            {
+                matrix[i][j] = 1;
+            }
+            else
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+void printMatrix()
+{
+    printf("\n\r Matrix table \n\r");
+    for (int i = 0;i < 10;i++)
+    {
+        for (int j = 0;j < 10;j++)
+        {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n\r");
+    }
+}
+int move(int dir)
+{
+    if (dir > 6 || dir < 0)
+        return 0;
+
+        return 1;
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+    initMatrix();
+    printMatrix();
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
